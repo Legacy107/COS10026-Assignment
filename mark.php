49bcb4f06863697a8bd9dc43a70748594a059a7a
Legacy107 <quocmai1107@gmail.com>
2022-05-05 23:22:22 +1000

    refactor: refactor and reformat mark.php

--- mark.php
+++ mark.php
@@ -7,159 +7,171 @@
         return $data;
     }
 
-    function GetData() {
-        $dataArray = array();
+    function sanitise_data($data_array) {
+        foreach ($data_array as &$input) {
+            if (gettype($input) == "array") {
+                foreach ($input as &$field) {
+                    $filed = sanitise_input($field);
+                }
+            } else {
+                $input = sanitise_input($input);
+            }
+        }
+        return $data_array;
+    }
+
+    // Validate
+    function validate_data($data_array) {
+        $err_msg = "";
+
+        if (!preg_match("/^[a-zA-Z\s-]+$/", $data_array["fname"])) {
+            $err_msg .= "<p>Only alpha letters and hyphen are allowed in your first name.</p>" ;
+        }
+        else if(strlen($data_array["fname"]) > 30)
+        {
+            $err_msg .= "<p>First name is too long, maximum is 30 characters.</p>";
+        }
+
+        if (!preg_match("/^[a-zA-Z\s-]+$/", $data_array["lname"])) {
+            $err_msg .= "<P>Only alpha letters and hyphen are allowed in your last name.</p>";
+        }
+        else if(strlen($data_array["lname"]) > 30)
+        {
+            $err_msg .= "<p>Last name is too long, maximum is 30 characters.</p>";
+        }
+
+        if ($data_array["sid"] == "") {
+            $err_msg .= "<p>You must enter your student id.</p>";
+        } else if (!is_numeric($data_array["sid"])) {
+            $err_msg .= "<p>Your student id must be a number.</p>";
+        } else if (!preg_match('/^(\d{7}|\d{10})$/', $data_array["sid"])) {
+            $err_msg .= "<p>Your student id must be between 7 to 10 digits.</p>";
+        }
+
+        return $err_msg;
+    }
+
+    function get_data() {
+        $data_array = array();
         if (isset($_POST["fname"])) { //text alpha
-            array_push($dataArray, $_POST["fname"]);
+            $data_array["fname"] = $_POST["fname"];
         }
 
         if (isset($_POST["lname"])) { //text alpha
-            array_push($dataArray, $_POST["lname"]);
+            $data_array["lname"] = $_POST["lname"];
         }
 
         if (isset($_POST["sid"])) { //text numeric
-            array_push($dataArray, $_POST["sid"]);
+            $data_array["sid"] = $_POST["sid"];
         }
 
         if (isset($_POST["MPEG"])) { //text alpha
-            array_push($dataArray, $_POST["MPEG"]);
+            $data_array["MPEG"] = $_POST["MPEG"];
         }
 
         if (isset($_POST["years"])) { //radio
-            array_push($dataArray, $_POST["years"]);
+            $data_array["years"] = $_POST["years"];
         }
 
         if (isset($_POST["standards"])) { //checkbox
-            array_push($dataArray, $_POST["standards"]);
+            $data_array["standards"] = $_POST["standards"];
         }
 
         if (isset($_POST["decline"])) { //checkbox
-            array_push($dataArray, $_POST["decline"]);
+            $data_array["decline"] = $_POST["decline"];
         }
 
         if (isset($_POST["creator"])) { //select
-            array_push($dataArray, $_POST["creator"]);
+            $data_array["creator"] = $_POST["creator"];
         }
 
         if (isset($_POST["compression"])) { //radio
-            array_push($dataArray, $_POST["compression"]);
+            $data_array["compression"] = $_POST["compression"];
         }
 
-        return $dataArray;
+        return $data_array;
     }
 
     // Mark data to points
-    function MarkQuestion($dataArray) {
-        $totalPoints = 0;
+    function mark_question($data_array) {
+        $total_points = 0;
 
-        if ($dataArray[3] == "Moving Picture Experts Group") {
-            $totalPoints += 1;
+        if ($data_array["MPEG"] == "Moving Picture Experts Group") {
+            $total_points += 1;
         }
         
-        if ($dataArray[4] == "1995 / 1998") {
-            $totalPoints += 1;
+        if ($data_array["years"] == "1995 / 1998") {
+            $total_points += 1;
         }
 
-        $pointStandards = 0;
-        for ($i=0; $i<count($dataArray[5]); $i++) {
-            if (($dataArray[5])[$i] == "MPEG 1 Audio layer III") {
-                $pointStandards += 0.5;
+        $point_standards = 0;
+        for ($i = 0; $i < count($data_array["standards"]); $i++) {
+            if ($data_array["standards"][$i] == "MPEG 1 Audio layer III") {
+                $point_standards += 0.5;
             }
 
-            if (($dataArray[5])[$i] == "MPEG 2 Audio Layer III") {
-                $pointStandards += 0.5;
+            if ($data_array["standards"][$i] == "MPEG 2 Audio Layer III") {
+                $point_standards += 0.5;
             }
 
-            if (($dataArray[5])[$i] == "MPEG 2.5 Audio Layer III") {
-                $pointStandards -= 0.5;
+            if ($data_array["standards"][$i] == "MPEG 2.5 Audio Layer III") {
+                $point_standards -= 0.5;
             }
 
-            if (($dataArray[5])[$i] == "MPEG 3 Audio Layer III") {
-                $pointStandards -= 0.5;
-            }
-        }
-        if ($pointStandards > 0) { //If have points, add to total points
-            $totalPoints += $pointStandards;
-        }
-
-        $pointDecline = 0;
-        for ($j=0; $j<count($dataArray[6]); $j++) {
-            if (($dataArray[6])[$j] == "variety of devices") {
-                $pointDecline += 0.5;
-            }
-
-            if (($dataArray[6])[$j] == "legal issues") {
-                $pointDecline -= 0.5;
-            }
-
-            if (($dataArray[6])[$j] == "alternative audio formats") {
-                $pointDecline += 0.5;
-            }
-
-            if (($dataArray[6])[$j] == "MPEG changes") {
-                $pointDecline -= 0.5;
+            if ($data_array["standards"][$i] == "MPEG 3 Audio Layer III") {
+                $point_standards -= 0.5;
             }
         }
-        if ($pointDecline > 0) { //If have points, add to total points
-            $totalPoints += $pointDecline;
+        if ($point_standards > 0) { //If have points, add to total points
+            $total_points += $point_standards;
         }
 
-        if ($dataArray[7] == "Fraunhofer Society") {
-            $totalPoints += 1;
-        }
-
-        if ($dataArray[8] == "False") {
-            $totalPoints += 1;
-        }
-
-        return $totalPoints;
-    }
-    
-    $data = GetData();
-
-    // Sanitize data
-    foreach ($data as &$input) {
-        if (gettype($input) == "array") {
-            foreach ($input as &$field) {
-                $filed = sanitise_input($field);
+        $point_decline = 0;
+        for ($i = 0; $i < count($data_array["decline"]); $i++) {
+            if ($data_array["decline"][$i] == "variety of devices") {
+                $point_decline += 0.5;
             }
-        } else {
-            $input = sanitise_input($input);
+
+            if ($data_array["decline"][$i] == "legal issues") {
+                $point_decline -= 0.5;
+            }
+
+            if ($data_array["decline"][$i] == "alternative audio formats") {
+                $point_decline += 0.5;
+            }
+
+            if ($data_array["decline"][$i] == "MPEG changes") {
+                $point_decline -= 0.5;
+            }
         }
+        if ($point_decline > 0) { //If have points, add to total points
+            $total_points += $point_decline;
+        }
+
+        if ($data_array["creator"] == "Fraunhofer Society") {
+            $total_points += 1;
+        }
+
+        if ($data_array["compression"] == "False") {
+            $total_points += 1;
+        }
+
+        return $total_points;
     }
 
-    // Validate Stuff
-    $errMsg = "";
-    if (!preg_match("/^[a-zA-Z\s-]{1,30}$/", $data[0])) {
-        $errMsg .= "<p>Only alpha letters allowed in your first name.</p>" ;
-    }
-    else if(strlen($data[0]) > 30)
-    {
-        $errMsg .= "<p>First name is too long, maximum is 30 characters.</p>";
-    }
 
-    if (!preg_match("/^[a-zA-Z\s-]{1,30}$/", $data[1])) {
-        $errMsg .= "<P>Only alpha letters allowed in your last name.</p>" ;
-    }
-    else if(strlen($data[1]) > 30)
-    {
-        $errMsg .= "<p>Last name is too long, maximum is 30 characters.</p>";
-    }
+    $data = get_data();
 
-    if ($data[2] == "") {
-        $errMsg .= "<p>You must enter your student id.</p>" ;
-    } else if (!is_numeric($data[2])) {
-        $errMsg .= "<p>Your student id must be a number.</p>" ;
-    } else if (!preg_match('/^(\d{7}|\d{10})$/', $data[2])) {
-        $errMsg .= "<p>Your student id must be between 7 to 10 digits.</p>" ;
-    }
+    $data = sanitise_data($data);
 
-    if ($errMsg) {
-        echo($errMsg);
+    $err_msg = validate_data($data);
+
+    if ($err_msg) {
+        echo($err_msg);
         exit();
     }
 
-    $points = MarkQuestion($data);
+    $points = mark_question($data);
 
     echo "$points<br>"; //delete later
 ?>
