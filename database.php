ff507393823acb3b507e92299b10f9bbe2927037
DynamoMarsh <100823857+DynamoMarsh@users.noreply.github.com>
2022-05-15 20:57:13 +1000

    quick change + indentation revert

--- database.php
+++ database.php
@@ -1,80 +1,76 @@
 
 <?php
-include "db_settings.php";
+    include "db_settings.php";
 
-# Gets a connection to the MySQL database using the config variables. Returns null if it can't connect.
-function get_conn()
-{
-    global $host, $user, $pwd, $sql_db;
-    try {
-        $conn = mysqli_connect($host, $user, $pwd, $sql_db);
-    } catch (Exception $_ex) {
-        return null;
+    # Gets a connection to the MySQL database using the config variables. Returns null if it can't connect.
+    function get_conn()
+    {
+        global $host, $user, $pwd, $sql_db;
+        try {
+            $conn = mysqli_connect($host, $user, $pwd, $sql_db);
+        } catch (Exception $_ex) {
+            return null;
+        }
+        return $conn;
     }
-    return $conn;
-}
 
-# Creates the 'admins' table if it doesn't exist.
-function create_admin_table($conn)
-{
-    $query = "CREATE TABLE IF NOT EXISTS admins (
-            id INT NOT NULL AUTO_INCREMENT,
-            username VARCHAR(30) NOT NULL UNIQUE,
-            password VARCHAR(30) NOT NULL,
-            PRIMARY KEY (id)
-        )";
-    try {
-        mysqli_query($conn, $query);
-    } catch (Exception $_ex) {
-        return false;
+    # Creates the 'admins' table if it doesn't exist.
+    function create_admin_table($conn)
+    {
+        $query = "CREATE TABLE IF NOT EXISTS admins (
+                id INT NOT NULL AUTO_INCREMENT,
+                username VARCHAR(30) NOT NULL UNIQUE,
+                password VARCHAR(30) NOT NULL,
+                PRIMARY KEY (id)
+            )";
+        try {
+            mysqli_query($conn, $query);
+        } catch (Exception $_ex) {
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-# Creates the 'users' table if it doesn't exist.
-function create_user_table($conn)
-{
-    $query = "CREATE TABLE IF NOT EXISTS users (
-            id INT NOT NULL,
-            firstname VARCHAR(30) NOT NULL,
-            lastname VARCHAR(30) NOT NULL,
-            PRIMARY KEY (id)
-        )";
-    try {
-        mysqli_query($conn, $query);
-    } catch (Exception $_ex) {
-        return false;
+    # Creates the 'users' table if it doesn't exist.
+    function create_user_table($conn)
+    {
+        $query = "CREATE TABLE IF NOT EXISTS users (
+                id INT NOT NULL,
+                firstname VARCHAR(30) NOT NULL,
+                lastname VARCHAR(30) NOT NULL,
+                PRIMARY KEY (id)
+            )";
+        try {
+            mysqli_query($conn, $query);
+        } catch (Exception $_ex) {
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-# Creates the 'attempts' table if it doesn't exist.
-function create_attempt_table($conn)
-{
-    $query = "CREATE TABLE IF NOT EXISTS attempts (
-            id INT NOT NULL AUTO_INCREMENT,
-            userId INT NOT NULL,
-            score TINYINT NOT NULL,
-            dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            dateUpdated TIMESTAMP,
-            PRIMARY KEY (id),
-            FOREIGN KEY (userId) REFERENCES users(id)
-        )";
-    try {
-        mysqli_query($conn, $query);
-    } catch (Exception $_ex) {
-        return false;
+    # Creates the 'attempts' table if it doesn't exist.
+    function create_attempt_table($conn)
+    {
+        $query = "CREATE TABLE IF NOT EXISTS attempts (
+                id INT NOT NULL AUTO_INCREMENT,
+                userId INT NOT NULL,
+                score TINYINT NOT NULL,
+                dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                dateUpdated TIMESTAMP,
+                PRIMARY KEY (id),
+                FOREIGN KEY (userId) REFERENCES users(id)
+            )";
+        try {
+            mysqli_query($conn, $query);
+        } catch (Exception $_ex) {
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-# Create a user.
-function save_user($conn, $sid, $fname, $lname)
-{
-    $query = "SELECT * FROM users WHERE id = " .  $sid;
-    $result = mysqli_query($conn, $query);
-    $rows = mysqli_num_rows($result);
-    if ($rows == 0) {
+    # Create a user.
+    function save_user($conn, $sid, $fname, $lname)
+    {
         $query = "INSERT INTO users (id, firstName, lastName)
             VALUES ('$sid', '$fname', '$lname')";
         try {
@@ -84,181 +80,178 @@ function save_user($conn, $sid, $fname, $lname)
         }
         return true;
     }
-}
 
-# Create an attempt.
-function save_attempt($conn, $user_id, $score)
-{
-    $query = "INSERT INTO attempts (userId, score, dateUpdated)
-            VALUES ('$user_id', $score, CURRENT_TIMESTAMP);
-        ";
-    try {
-        mysqli_query($conn, $query);
-    } catch (Exception $_ex) {
-        return false;
-    }
-    return true;
-}
-
-# Read attempts.
-function get_attempts($conn, $user_id = null, $fname = null, $lname = null)
-{
-    $need_and = false;
-    $query = "SELECT attempts.*, users.firstname, users.lastname FROM attempts
-            INNER JOIN users ON attempts.userId = users.id
-        ";
-    if ($user_id) {
-        if ($need_and)
-            $query .= " and";
-        else {
-            $need_and = true;
-            $query .= " WHERE";
-        }
-
-        $query .= " users.id = '$user_id'";
-    }
-    if ($fname) {
-        if ($need_and)
-            $query .= " and";
-        else {
-            $need_and = true;
-            $query .= " WHERE";
-        }
-
-        $query .= " users.firstname = '$fname'";
-    }
-    if ($lname) {
-        if ($need_and)
-            $query .= " and";
-        else {
-            $need_and = true;
-            $query .= " WHERE";
-        }
-
-        $query .= " users.lastname = '$lname'";
-    }
-
-    $query .= "ORDER BY attempts.dateCreated, attempts.id";
-
-    try {
-        $result = mysqli_query($conn, $query);
-        if (!$result)
+    # Create an attempt.
+    function save_attempt($conn, $user_id, $score)
+    {
+        $query = "INSERT INTO attempts (userId, score, dateUpdated)
+                VALUES ('$user_id', $score, CURRENT_TIMESTAMP);
+            ";
+        try {
+            mysqli_query($conn, $query);
+        } catch (Exception $_ex) {
             return false;
+        }
+        return true;
+    }
 
-        $attempts = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            array_push($attempts, $row);
+    # Read attempts.
+    function get_attempts($conn, $user_id = null, $fname = null, $lname = null)
+    {
+        $need_and = false;
+        $query = "SELECT attempts.*, users.firstname, users.lastname FROM attempts
+                INNER JOIN users ON attempts.userId = users.id";
+        if ($user_id) {
+            if ($need_and)
+                $query .= " and";
+            else {
+                $need_and = true;
+                $query .= " WHERE";
+            }
+
+            $query .= " users.id = '$user_id'";
+        }
+        if ($fname) {
+            if ($need_and)
+                $query .= " and";
+            else {
+                $need_and = true;
+                $query .= " WHERE";
+            }
+
+            $query .= " users.firstname = '$fname'";
+        }
+        if ($lname) {
+            if ($need_and)
+                $query .= " and";
+            else {
+                $need_and = true;
+                $query .= " WHERE";
+            }
+
+            $query .= " users.lastname = '$lname'";
         }
 
-        mysqli_free_result($result);
-        return $attempts;
-    } catch (Exception $_ex) {
-        return false;
-    }
-}
+        $query .= "ORDER BY attempts.dateCreated, attempts.id";
 
-# Read second attempts with score < 50%.
-function get_second_attempts_lt_50($conn)
-{
-    $query = "SELECT attempts.*, users.firstname, users.lastname FROM attempts
-            INNER JOIN users ON attempts.userId = users.id
-            INNER JOIN
-            (
-                SELECT MAX(dateCreated) maxDateCreated, userId, COUNT(id) AS numberOfAttempts
-                FROM attempts
-                GROUP BY userId
-                HAVING numberOfAttempts = 2
-            ) temp_attempts
-            ON attempts.userId = temp_attempts.userId
-            AND attempts.dateCreated = temp_attempts.maxDateCreated
-            WHERE attempts.score < 3
-            ORDER BY attempts.dateCreated, attempts.id
-        ";
+        try {
+            $result = mysqli_query($conn, $query);
+            if (!$result)
+                return false;
 
-    try {
-        $result = mysqli_query($conn, $query);
-        if (!$result)
+            $attempts = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($attempts, $row);
+            }
+
+            mysqli_free_result($result);
+            return $attempts;
+        } catch (Exception $_ex) {
             return false;
-
-        $attempts = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            array_push($attempts, $row);
         }
-
-        mysqli_free_result($result);
-        return $attempts;
-    } catch (Exception $_ex) {
-        return false;
     }
-}
 
-# Read first attempts with scores of 100%.
-function get_first_attempts_100($conn)
-{
-    $query = "SELECT attempts.*, users.firstname, users.lastname FROM attempts
-            INNER JOIN users ON attempts.userId = users.id
-            INNER JOIN
-            (
-                SELECT min(dateCreated) minDateCreated, userId
-                FROM attempts
-                GROUP BY userId
-            ) temp_attempts
-            ON attempts.userId = temp_attempts.userId
-            AND attempts.dateCreated = temp_attempts.minDateCreated
-            WHERE attempts.score = 6
-            ORDER BY attempts.dateCreated, attempts.id
-        ";
+    # Read second attempts with score < 50%.
+    function get_second_attempts_lt_50($conn)
+    {
+        $query = "SELECT attempts.*, users.firstname, users.lastname FROM attempts
+                INNER JOIN users ON attempts.userId = users.id
+                INNER JOIN
+                (
+                    SELECT MAX(dateCreated) maxDateCreated, userId, COUNT(id) AS numberOfAttempts
+                    FROM attempts
+                    GROUP BY userId
+                    HAVING numberOfAttempts = 2
+                ) temp_attempts
+                ON attempts.userId = temp_attempts.userId
+                AND attempts.dateCreated = temp_attempts.maxDateCreated
+                WHERE attempts.score < 3
+                ORDER BY attempts.dateCreated, attempts.id
+            ";
 
-    try {
-        $result = mysqli_query($conn, $query);
-        if (!$result)
+        try {
+            $result = mysqli_query($conn, $query);
+            if (!$result)
+                return false;
+
+            $attempts = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($attempts, $row);
+            }
+
+            mysqli_free_result($result);
+            return $attempts;
+        } catch (Exception $_ex) {
             return false;
-
-        $attempts = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            array_push($attempts, $row);
         }
-
-        mysqli_free_result($result);
-        return $attempts;
-    } catch (Exception $_ex) {
-        return false;
     }
-}
 
-# Update an attempt.
-function update_attempt($conn, $id, $score)
-{
-    $query = "UPDATE attempts
-            SET score = $score, dateUpdated = CURRENT_TIMESTAMP
-            WHERE id = '$id'
-        ";
-    try {
-        mysqli_query($conn, $query);
-    } catch (Exception $_ex) {
-        return false;
+    # Read first attempts with scores of 100%.
+    function get_first_attempts_100($conn)
+    {
+        $query = "SELECT attempts.*, users.firstname, users.lastname FROM attempts
+                INNER JOIN users ON attempts.userId = users.id
+                INNER JOIN
+                (
+                    SELECT min(dateCreated) minDateCreated, userId
+                    FROM attempts
+                    GROUP BY userId
+                ) temp_attempts
+                ON attempts.userId = temp_attempts.userId
+                AND attempts.dateCreated = temp_attempts.minDateCreated
+                WHERE attempts.score = 6
+                ORDER BY attempts.dateCreated, attempts.id
+            ";
+
+        try {
+            $result = mysqli_query($conn, $query);
+            if (!$result)
+                return false;
+
+            $attempts = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($attempts, $row);
+            }
+
+            mysqli_free_result($result);
+            return $attempts;
+        } catch (Exception $_ex) {
+            return false;
+        }
     }
-    return true;
-}
 
-# Delete attempts.
-function delete_attempts($conn, $ids)
-{
-    $query = "DELETE FROM attempts
+    # Update an attempt.
+    function update_attempt($conn, $id, $score)
+    {
+        $query = "UPDATE attempts
+                SET score = $score, dateUpdated = CURRENT_TIMESTAMP
+                WHERE id = '$id'";
+        try {
+            mysqli_query($conn, $query);
+        } catch (Exception $_ex) {
+            return false;
+        }
+        return true;
+    }
+
+    # Delete attempts.
+    function delete_attempts($conn, $ids)
+    {
+        $query = "DELETE FROM attempts
             WHERE id IN (" . implode(", ", $ids) . ")";
-    try {
-        mysqli_query($conn, $query);
-    } catch (Exception $_ex) {
-        return false;
+        try {
+            mysqli_query($conn, $query);
+        } catch (Exception $_ex) {
+            return false;
+        }
+        return true;
     }
-    return true;
-}
 
-$conn = get_conn();
-if ($conn) {
-    create_user_table($conn);
-    create_attempt_table($conn);
-    create_admin_table($conn);
-    mysqli_close($conn);
-}
+    $conn = get_conn();
+    if ($conn) {
+        create_user_table($conn);
+        create_attempt_table($conn);
+        create_admin_table($conn);
+        mysqli_close($conn);
+    }
 ?>
--- db_settings.php
+++ db_settings.php
@@ -1,10 +1,10 @@
 
 <?php
-# MariaDB config variables.
-$host = "feenix-mariadb.swin.edu.au";
-$user = "s103575527";
-$pwd = "311002";
-$sql_db = "s103575527_db";
-# Details for Orson's database.
-# Has [orson_routt, long_password] and [admin, not_admin] as logins.
+    # MariaDB config variables.
+    $host = "feenix-mariadb.swin.edu.au";
+    $user = "s103575527";
+    $pwd = "311002";
+    $sql_db = "s103575527_db";
+    # Details for Orson's database.
+    # Has [orson_routt, long_password] and [admin, not_admin] as logins.
 ?>
